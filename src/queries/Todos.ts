7d5b842b42672dc228b2f07e37fc1b121994aa6f
Tom Poidevin <tom.poidevin@gmail.com>
2023-01-19 18:50:20 +0100

    fix api call

--- src/queries/Todos.ts
+++ src/queries/Todos.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-export const getTodos = () => axios.get(`${import.meta.env.VITE_BASE_DB_URL}/todos`)
-    .then((res) => res.data)
+// export const getTodos = () => axios.get(`${import.meta.env.VITE_BASE_DB_URL}/todos`)
+//     .then((res) => res.data)
+
+export const getTodos = () => axios({
+    method: 'get',
+    url: `${import.meta.env.VITE_BASE_DB_URL}/todos`,
+    withCredentials: false,
+}).then((res) => res.data)
 
 export default {
     getTodos
