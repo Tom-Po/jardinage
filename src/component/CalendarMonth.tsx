5453124a263e6ea777191cae86e3ead99be9fa1a
Tom Poidevin <tom.poidevin@gmail.com>
2023-01-21 15:05:08 +0100

    mobile nav and toggle complete on calendar

--- src/component/CalendarMonth.tsx
+++ src/component/CalendarMonth.tsx
@@ -1,10 +1,11 @@
 import styles from './CalendarMonth.module.css'
 import { SeedType } from './Seed'
 import { ReactComponent as SeedSVG } from '../assets/Seed.svg'
-import { useQuery } from 'react-query'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 import { getTodos } from '../queries/Todos'
 import { TodoType } from './Todo'
 import date from '../constant/Date'
+import axios from 'axios'
 
 interface ICalendarMonth {
     name: string,
@@ -12,9 +13,26 @@ interface ICalendarMonth {
 }
 const CalendarMonth: React.FC<ICalendarMonth> = ({ name, availableSeeds }) => {
     const { data: todos = [] } = useQuery<TodoType[]>("todos", getTodos)
+    const queryClient = useQueryClient()
+
     const currentMonthTodos = todos.filter(todo => {
         return todo.month === date.MONTHS.findIndex(month => month === name)
-    }).sort((a, b) => a.isCompleted > b.isCompleted || a.text > b.text ? 1 : -1)
+    }).sort((a, b) => a.isCompleted > b.isCompleted ? 1 : -1)
+
+    const setCompletedTodo = useMutation({
+        mutationFn: (todo: TodoType) =>
+            axios.put(`${import.meta.env.VITE_BASE_DB_URL}/todos/${todo.id}`, {
+                ...todo,
+                isCompleted: !todo.isCompleted
+            }),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['todos'] })
+        },
+    })
+
+    const completeTodo = (todo: TodoType) => {
+        setCompletedTodo.mutate(todo)
+    }
 
     return (
         <div key={name} className={styles.Month}>
@@ -22,22 +40,28 @@ const CalendarMonth: React.FC<ICalendarMonth> = ({ name, availableSeeds }) => {
             <div className={styles.AvailableSeeds}>
                 {availableSeeds.map(seed => (
                     <div key={seed.name} className={styles.Seed}>
-                        <SeedSVG />
+                        <div className={styles.Icon}><SeedSVG /></div>
                         <div>{seed.name}</div>
                     </div>
                 ))}
 
             </div>
-            {currentMonthTodos.map(todo => (
-                <div key={todo.text} className={styles.Seed}>
-                    {todo.isCompleted && <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                        </svg>
-                    </div>}
-                    <div>{todo.text}</div>
-                </div>
-            ))}
+            <div className={styles.TodoList}>
+                {currentMonthTodos.map(todo => (
+                    <div key={todo.text} className={`${styles.Seed} ${styles.Todo}`} onClick={() => completeTodo(todo)}>
+                        <div className={styles.Todo}>
+                            {todo.isCompleted && (
+                                <div className={styles.Icon}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                    </svg>
+                                </div>
+                            )}
+                        </div>
+                        <div>{todo.text}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
---
+++ src/component/NavigationMobile.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { routes } from '../routes/routes'
+import styles from './NavigationMobile.module.css'
+
+const NavigationMobile = () => {
+    const [open, setOpen] = useState(false)
+    const toggleOpen = () => {
+        setOpen(!open)
+    }
+
+    return (
+        <div>
+            <div className={styles.MenuBtn} onClick={toggleOpen}>
+                <div className={`${styles.Burger} ${open && styles.Open}`}>
+                </div>
+            </div>
+            {open && (
+                <div className={styles.MobileNav} onClick={toggleOpen}>
+                    {routes.map((route) => (
+                        <Link key={route.path} to={route.path}>{route.name}</Link>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default NavigationMobile
