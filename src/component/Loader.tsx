f49d3c9dd7ef006b5931f27d23100d57a83167a1
Tom Poidevin <tom.poidevin@gmail.com>
2023-01-22 17:09:00 +0100

    fix loaderé

--- src/component/Loader.tsx
+++ src/component/Loader.tsx
@@ -3,7 +3,7 @@ import styles from './Loader.module.css'
 const Loader = () => {
     return (
         <div>
-            <div className={styles.Loader}="loader loader-2"/>
+            <div className={styles.Loader} />
         </div>
     )
 }
