ec16148b0fed541a5f61dc5258e669d23d025287
Tom Poidevin <tom.poidevin@gmail.com>
2023-03-20 13:44:52 +0100

    export module

--- src/component/SeedList.tsx
+++ src/component/SeedList.tsx
@@ -3,6 +3,7 @@ import { useMutation, useQuery, useQueryClient } from "react-query";
 import { getSeeds } from '../queries/Seeds';
 import Seed, { SeedType } from "./Seed";
 import styles from './SeedList.module.css';
+import { ReactComponent as Export } from '../assets/Export.svg';
 
 const SeedList = () => {
     const queryClient = useQueryClient()
@@ -34,6 +35,23 @@ const SeedList = () => {
         },
     })
 
+    const exportAllSeeds = () => {
+        let header = ''
+        const cleanedObjects = seeds.map((s: SeedType) => {
+            delete s.images
+            if (!header.length) {
+                header = Object.keys(s).map(key => key).join(',') + '\n'
+            }
+            return Object.values(s).map(value => Array.isArray(value) ? value.join('|') : value)
+        }).sort((a: any, b: any) => a.id > b.id)
+
+        let csvContent = "data:text/csv;charset=utf-8," 
+        + header
+        + cleanedObjects.map((e: any) => e.join(",")).join("\n");
+        let encodedUri = encodeURI(csvContent);
+        window.open(encodedUri);
+    }
+
     return (
         <div className={styles.Seeds}>
             {seeds && seeds.length
@@ -42,6 +60,11 @@ const SeedList = () => {
                     } seed={seed} />
                 ))
                 : <div>No seeds</div>}
+            {seeds && (
+                <div className={styles.ExportButton} onClick={exportAllSeeds}>
+                    <Export />    
+                </div>
+            )}
         </div>
     )
 }
